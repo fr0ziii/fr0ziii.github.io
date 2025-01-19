3e5586d411d5c6eb347834d2aaf36c723d056777
David Iglesias <fr0ziisc2@gmail.com>
2025-01-19 15:10:35 +0000

    Update Astro configuration and GitHub Actions workflow
    - Added React integration to the Astro configuration.
    - Updated sitemap URLs to reflect the new site structure.
    - Changed output mode from 'server' to 'static' in astro.config.mjs.
    - Updated package.json to include React and its types, along with gh-pages for deployment.
    - Refined GitHub Actions workflow for deployment to GitHub Pages, simplifying the process and ensuring compatibility with Node.js 18.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,36 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
-
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fr0ziii.github.io",
-  integrations: [sitemap(), tailwind(), robotsTxt({
-    sitemap: [
-      "https://gianmarcocavallo.com/sitemap-index.xml",
-      "https://gianmarcocavallo.com/sitemap-0.xml",
-    ],
-  }), solidJs(), UnoCSS({ injectReset: true }), icon(), svelte(), tailwind()],
+  output: "static",
+  integrations: [
+    sitemap(),
+    tailwind(),
+    solidJs(),
+    robotsTxt({
+      sitemap: [
+        "https://fr0ziii.github.io/sitemap-index.xml",
+        "https://fr0ziii.github.io/sitemap-0.xml",
+      ],
+    }),
+    UnoCSS({ injectReset: true }),
+    icon(),
+    svelte(),
+    react(),
+  ],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
-  output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
   vite: {
     assetsInclude: "**/*.riv",
   },
